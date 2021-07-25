170deebf509e87d861810f93e3eb9d63bbef0929
Marcus Cvjeticanin <marcuscvjeticanin@gmail.com>
2021-07-25 15:58:14 +0200

    Adding docker-compose

--- src/main/java/com/mjovanc/coinshark/CoinsharkApplication.java
+++ src/main/java/com/mjovanc/coinshark/CoinsharkApplication.java
@@ -2,11 +2,12 @@ package com.mjovanc.coinshark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class CoinsharkApplication {
+public class CoinsharkApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CoinsharkApplication.class, args);
 	}
